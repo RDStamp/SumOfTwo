f2be13af71d7ce6f878eaae460f590cbde112cda
rufus-coder <153910025+rufus-coder@users.noreply.github.com>
2025-05-30 12:00:25 +0100

    Add project files.

---
+++ SumOfTwo/Exercise.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwo
+{
+    public class Exercise
+    {
+        public static int SumOfTwo(int[] nums, int SumToFind)
+        {
+            List<Sums> sums = [];
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] + nums[i + 1] == SumToFind)
+                {
+                    sums.Add(new Sums(nums[i], nums[i + 1]));
+                }
+            }
+
+            return sums.Distinct().Count();
+        }
+    }
+
+    public struct Sums
+    {
+        public Sums(int value1, int value2)
+        {
+            v1 = value1;
+            v2 = value2;
+            v3 = value1 + value2;
+        }
+
+        public int v1 = 0;
+        public int v2 = 0;
+        public int v3 = 0;
+    }
+}
---
+++ SumOfTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SumOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = Exercise.SumOfTwo([1, 1, 1, 2, 3, 4, 5, 2], 2);
+            Console.WriteLine(response);
+
+            response = Exercise.SumOfTwo([2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 1, 4, 1, 4, 5, 0, 5, 0], 5);
+            Console.WriteLine(response);
+        }
+    }
+}
